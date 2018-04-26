89495112d61837b41df24ff5d5ef1abd41cb95d7
schlomo <schlomocohen@hotmail.com>
2018-04-26 16:57:38 +0300

    THE MAIN :
      MAKE CONNECTION TO THE SERVER
      DOWNLOAD THE FILE WITH THE MISSIONS

--- RaceOfYedidim/Main.java
+++ RaceOfYedidim/Main.java
@@ -17,6 +17,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import org.apache.commons.*;
 
 
 
@@ -28,7 +29,7 @@ public class Main {
     private int teamCode;
     private String language[];
     private String nameOfSchool;
-    private InetAddress serversIp;
+        private InetAddress serversIp;
     private double coordination;
     private Object Mission [];
     private int timeOutNavigate;
@@ -55,7 +56,7 @@ public class Main {
     public void popMessageToPlayer() {
         // TODO implement here
     }
-    public void getDataFromServer() {
+    public void getDataFromServer() {  ///
         //  how to get data from  socket and write to .
 //  OutputStream outToServer = client.getOutputStream();
 //  DataOutputStream out = new DataOutputStream(outToServer);
@@ -87,6 +88,8 @@ public class Main {
     }
     public void getMaps() {
         // TODO implement here
+
+
     }
     public void timerUpdateLocation() {
         // TODO implement here
@@ -103,13 +106,6 @@ public class Main {
             Socket client = new Socket(serverName, port);
 
             System.out.println("Just connected to " + client.getRemoteSocketAddress());
-            //  how to get data from  socket and write to .
-//     OutputStream outToServer = client.getOutputStream();
-//     DataOutputStream out = new DataOutputStream(outToServer);
-//
-//     out.writeUTF("Hello from " + client.getLocalSocketAddress());
-//     InputStream inFromServer = client.getInputStream();
-//     DataInputStream in = new DataInputStream(inFromServer);
             serversIp=InetAddress.getLocalHost();
 
         } catch (UnknownHostException e ){
@@ -129,3 +125,150 @@ public class Main {
 
 
     }}
+
+
+
+
+
+
+//
+//    public void retrieve(String server, int port, String username,
+//                         String password, String remote, String local, int fileType,
+//                         ProgressHandler progressHandler) throws Exception {
+//        final FTPClient ftp = new FTPClient();
+//        Date date = new Date();
+//        long startTime_ms = date.getTime();
+//        if (progressHandler != null) {
+//            ftp.setCopyStreamListener(new FtpCopyStreamListener(progressHandler));
+//        }
+//
+//        ftpConnect(server,ftp, port,startTime_ms);
+//
+//
+//        if(ftp.getReplyCode()==0 || !String.valueOf(ftp.getReplyCode()).startsWith("2")){
+//            cleanup(ftp, "Could not log into server: " + server, null);
+//            return;
+//        }
+//
+//        boolean loggedIn = false;
+//        try {
+//            if (username == null || username.isEmpty()) {
+//                username = "anonymous";
+//                password = System.getProperty("user.name") + "@"
+//                        + InetAddress.getLocalHost().getHostName();
+//            }
+//            if (!ftp.login(username, password)) {
+//                ftp.logout();
+//                cleanup(ftp, "Could not log into server: " + server, null);
+//            }
+//            loggedIn = true;
+//
+//            ftp.setFileType(fileType);
+//            ftp.enterLocalPassiveMode();
+//            OutputStream output = null;
+//            try {
+//                output = new FileOutputStream(local);
+//                LOGGER.info("About to download " + remote + " from " + server
+//                        + " on " + (port > 0 ? port : ftp.getDefaultPort())
+//                        + " to " + local);
+//                ftp.setControlKeepAliveTimeout(300);
+//                boolean isFileDownloaded = false;
+//                try{
+//                    isFileDownloaded = ftp.retrieveFile(remote, output);
+//
+//                }
+//                catch(IOException ex){
+//                    if(ftp.isConnected()){
+//
+//                        try{
+//                            int retryCode = 0;
+//                            while (!String.valueOf(retryCode).startsWith("2") && (new Date().getTime() < (startTime_ms + TOTAL_DURATION_MSECS))){
+//                                try{
+//
+//                                    retryCode = ftp.retr(local);
+//                                }catch(Exception e1){
+//                                    Thread.sleep(1000);
+//                                    System.out.println("File not downloaded !! " + e1.toString());
+//                                    ftpConnect(server, ftp, port, startTime_ms);
+//                                }
+//
+//                            }
+//                        }catch(Exception e){
+//                            //ftp.getReplyCode()
+//                            //throw e;
+//                            //disconnect(ftp);
+//                            //ftpConnect(server, ftp, port, startTime_ms);
+//
+//                            System.out.println("File not downloaded !! " + e.toString());
+//                        }
+//                    }
+//                }
+//                if(isFileDownloaded){
+//                    LOGGER.info("Finished downloading " + remote + " from "
+//                            + server + " on "
+//                            + (port > 0 ? port : ftp.getDefaultPort()) + " to "
+//                            + local);
+//                }
+//                else{
+//                    System.out.println("File not downloaded !! ");
+//                }
+//
+//            }catch(IOException io){
+//                io.printStackTrace();
+//
+//                throw io;
+//            }
+//            finally {
+//                if (output != null) {
+//                    try {
+//                        output.close();
+//                    } catch (IOException f) {
+//                        LOGGER.severe("output.close() error: "
+//                                + ServiceUtils.stackToString(f));
+//                    }
+//                }
+//            }
+//        }
+//        catch (FTPConnectionClosedException e) {
+//            LOGGER.severe("Server closed connection: " + server + " "
+//                    + ServiceUtils.stackToString(e));
+//            throw e;
+//        } catch (IOException e) {
+//            LOGGER.severe("IOException, server: " + server + " "
+//                    + ServiceUtils.stackToString(e));
+//            throw e;
+//        } finally {
+//            if (loggedIn) {
+//                try {
+//                    ftp.logout();
+//                } catch (IOException f) {
+//                    LOGGER.severe("ftp.logout() error: "
+//                            + ServiceUtils.stackToString(f));
+//                }
+//            }
+//            disconnect(ftp);
+//        }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
